f906dc9c3ca499ef311a545795044bec22ef9e50
rizgust <rizgust.dev@gmail.com>
2022-11-28 01:23:33 +0700

    [ADD] labels on text input

--- src/components/text-input/text-input.tsx
+++ src/components/text-input/text-input.tsx
@@ -6,18 +6,27 @@ interface TextInputProps {
   placeholder?: string;
   name?: string;
   id?: string;
+  label?: string;
+  message?: string;
+  value?: string;
   status: string;
-  onChange$?: PropFunction<(evt: any) => void>;
+  type: string;
+  onChange$?: PropFunction<(evt: any) => any>;
 }
 
 export const TextInput = component$((props: TextInputProps) => {
   return (
-    <input
-      type="text"
-      placeholder={props.placeholder}
-      class={`input w-full max-w-xs ${props.status == "error"  ? "border-red-500" : ""}`}
-      {...props}
-      onChange$={props.onChange$}
-    />
+    <div className="form-control w-full max-w-xs">
+      <label className="label">
+        <span className="label-text">{props.label}</span>
+      </label>
+      <input
+        placeholder={props.placeholder}
+        className={`input input-md input-bordered w-full max-w-xs" ${props.status == "error"  ? "input-error" : ""}`}
+        {...props}
+        onChange$={props.onChange$}
+      />
+        <span className={`label-text-alt italic ${props.status == "error"  ? "text-error" : ""}`}>{props.message}</span>
+    </div>
   );
 });
